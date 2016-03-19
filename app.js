156d73eb1915ddefc74a64b10a35ff7085b80566
Andrew Fry <andyfry01@gmail.com>
2016-03-19 15:22:09 -0400

    have the haversine formula, now to get it to call correctly..

--- app.js
+++ app.js
@@ -52,12 +52,12 @@ var findLatLong = function(origin, destination) {
     url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + origin + "&bounds=40.667219,-74.030623|40.808685,-73.910987&key=" + apiKey,
     dataType: "json",
     success: function(data) {
-      console.log("origin latitude is as follows:")
+      console.log("origin latitude is as follows:");
       console.log(data["results"][0]["geometry"]["location"]["lat"]);
-      console.log("origin longitude is as follows:")
+      console.log("origin longitude is as follows:");
       console.log(data["results"][0]["geometry"]["location"]["lng"]);
-      originCoords["lat"] = data["results"][0]["geometry"]["location"]["lat"]
-      originCoords["lng"] = data["results"][0]["geometry"]["location"]["lng"]
+      originCoords["lat"] = data["results"][0]["geometry"]["location"]["lat"];
+      originCoords["lng"] = data["results"][0]["geometry"]["location"]["lng"];
       console.log(originCoords)
     }
   })
@@ -66,28 +66,54 @@ var findLatLong = function(origin, destination) {
     url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + destination + "&bounds=40.667219,-74.030623|40.808685,-73.910987&key=" + apiKey,
     callback: JSON,
     success: function(data) {
-      console.log("destination latitude is as follows:")
+      console.log("destination latitude is as follows:");
       console.log(data["results"][0]["geometry"]["location"]["lat"]);
-      console.log("destination longitude is as follows:")
+      console.log("destination longitude is as follows:");
       console.log(data["results"][0]["geometry"]["location"]["lng"]);
-      destinationCoords["lat"] = data["results"][0]["geometry"]["location"]["lat"]
-      destinationCoords["lng"] = data["results"][0]["geometry"]["location"]["lng"]
+      destinationCoords["lat"] = data["results"][0]["geometry"]["location"]["lat"];
+      destinationCoords["lng"] = data["results"][0]["geometry"]["location"]["lng"];
       console.log(destinationCoords)
     }
   })
+  console.log("the distance between the start/end points is: ")
+  haversine(originCoords.lat, originCoords.lng, destinationCoords.lat, destinationCoords.lng)
+}
+//converts lat and lng to radians
+//Haversine Formula
+var haversine = function(lat1, lng1, lat2, lng2) {
+  var R = 6371000;
+  var φ1 = lat1.toRadians();
+  var φ2 = lat2.toRadians();
+  var Δφ = (lat2 - lat1).toRadians();
+  var Δλ = (lng2 - lng1).toRadians();
+
+  var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+    Math.cos(φ1) * Math.cos(φ2) *
+    Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  var d = R * c;
+
+  return d;
 }
 
+Number.prototype.toRadians = function() {
+    return this * Math.PI / 180;
+  }
 
-// var findCitiBike = function() {
-//   $.ajax({
-//     url: "http://alloworigin.com/get?url=http://www.citibikenyc.com/stations/json",
-//     // callback: "callback",
-//     // dataType: 'JSONP',
-//     success: function(data){
-//       console.log("here's the citibike data")
-//       console.log(data)
-//     }
-//   })
-// }
-//
-// findCitiBike();
+
+var findCitiBike = function() {
+  $.ajax({
+    url: "http://api.citybik.es/citi-bike-nyc.json",
+    dataType: "jsonp",
+    jsonpCallback: 'callback',
+  }).done(function(data) {
+    console.log(data)
+  });
+}
+
+findCitiBike();
+
+//convert citibike coords to correct format
+//going to need both .parseInt() and .toString()
+//var output = [num.slice(0, 2), ".", num.slice(2)].join('');
