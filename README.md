4b12eb78cc693c05eae250af258678cba63ce978
Andrew Fry <andyfry01@gmail.com>
2016-03-20 20:20:52 -0400

    version 1.0 completed.

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-Hello world!
+# Citi Bike Navigation App
+### An app combining Citi Bike station data and Google Maps.
+
+**App summary:**
+The app takes live Citi Bike station data and combines it with Google Maps walking and cycling directions
+to create step-by-step directions for getting from your origin point to your destination via Citi Bike. After entering the addresses of where you're departing from and where you're traveling to, the app first queries the Citi Bike API to find the closest station to your origin point that currently has bikes available. It then provides walking instructions to that station, maps out cycling directions between that Citi Bike station and the station closest to your destination point that has docking stations free, and finally gives you walking directions from that Citi Bike station to your destination. In the left-hand column of the page, the step-by-step directions are listed out for each step of the process (walking - cycling - walking), and the addresses and current status of each Citi Bike station are also listed.
+
+**Process:**
+The most challenging part of the whole process was integrating Google Maps into the app. The Google Maps API is really massive, so it took some work to figure out what was possible with the API and how to best integrate the features I needed into the app. The documentation on the Citi Bike API was a little sparse, so it took some trial and error before I was finally able to gain access to the data. There was also some work to be done in terms of taking the user input and feeding it into the various parts of the app so the individual components could function properly. But once I had assembled all the pieces I needed it was really just a matter of tweaking the outputs and the order everything was called in before I had a functioning app! Seeing it run successfully the first time was pretty satisfying.
+
+**Technologies used:**
+Handlebars, jQuery, the Google Maps API and the Citi Bike API were the primary technologies used.
+
+**Hangups/things I'd like to improve on**
+This first draft of the code is definitely not the cleanest or most eloquent JS I've ever written. If I had a few more days I could definitely refactor everything into smaller functions and get rid of a lot of extraneous material; as it is the code is pretty bad in terms of DRY. Ultimately, however, it works. I'd also like to spend some time styling of course.
+
+There's also a ton of room to expand in terms of functionality. Right now you have to be pretty literal when entering the origin and destination addresses so Google Maps doesn't try to send you to Brooklyn, Wisconsin or somewhere crazy, there isn't much in the way of map markers or waypoints or other visual cues, it would be cool if you could click on your start and end points on the map instead of typing them in manually, I'd like to find some way to disable the cycling instructions layer seeing as all the green on the page is a little distracting, so on and so forth. But I did manage to get the basic elements all up on the page and working as they should.
+
+**Resources:**
+
+[Google Maps JavaScript API](https://developers.google.com/maps/documentation/javascript/?hl=en_US)
+
+[Google Maps Geocoding API](https://developers.google.com/maps/documentation/geocoding/intro?hl=en_US)
+
+[Rendering multiple travel modes on one map](http://stackoverflow.com/questions/22922856/multiple-travel-modes-to-render-one-map-using-google-maps-api)
+
+[Haversine formula for calculating distances between lat/lng points](http://www.movable-type.co.uk/scripts/latlong.html)
+
+[Method for converting lat/lng to radians](http://stackoverflow.com/questions/5260423/torad-javascript-function-throwing-error)
+
+[Adding decimals to Citi Bike lat/lng coordinates](http://stackoverflow.com/questions/4364881/inserting-string-at-position-x-of-another-string)
--- app.js
+++ app.js
@@ -2,17 +2,50 @@ var start = document.getElementById("start");
 var end = document.getElementById("end");
 var submit = document.getElementById("submit");
 
+var handleBarsContainer = document.getElementById("handlebars-container")
+
+//handlebars source data
+var stationData = {
+  originName: undefined,
+  numBikes: undefined,
+  destinationName: undefined,
+  numSlots: undefined
+}
+
+//Variables used in locating Citibike stations
+var stationLat;
+var stationLng;
+
+var originStationLat;
+var originStationLng;
+var fullOriginCoords;
+
+var destinationStationLat;
+var destinationStationLng;
+var fullDestinationCoords;
+
+var haversineResult = undefined;
+var shortestOriginDistance = 10000;
+var shortestDestinationDistance = 10000;
+
+//Coordinates from origin and destination addresses, used in Haversine formula
+var originCoords = {
+  lat: "",
+  lng: ""
+}
+var destinationCoords = {
+  lat: "",
+  lng: ""
+}
+
+
 submit.addEventListener("click", function() {
   origin = start.value;
   destination = end.value;
   findLatLong(origin, destination);
 })
 
-$(document).ajaxStop(function() {
-  initMap();
-})
-
-
+//turns origin and destination addresses into coordinates for Haversine formula
 var findLatLong = function(origin, destination) {
   $.ajax({
     url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + origin + "&bounds=40.667219,-74.030623|40.808685,-73.910987&key=" + apiKey,
@@ -40,37 +73,12 @@ var findLatLong = function(origin, destination) {
       destinationCoords["lng"] = data["results"][0]["geometry"]["location"]["lng"];
       console.log(destinationCoords)
     }
-  }).done(function(){
+  }).done(function() {
     findCitiBike();
   })
 }
 
-var originCoords = {
-  lat: "",
-  lng: ""
-}
-
-var destinationCoords = {
-  lat: "",
-  lng: ""
-}
-
-var stationLat;
-var stationLng;
-
-var originStationLat;
-var originStationLng;
-var fullOriginCoords;
-
-var destinationStationLat;
-var destinationStationLng;
-var fullDestinationCoords = destinationStationLat + ", " + destinationStationLng;
-
-var haversineResult = undefined;
-var shortestOriginDistance = 10000;
-var shortestDestinationDistance = 10000;
-
-
+//Queries Citibike data, calls findClosestStation
 var findCitiBike = function() {
   $.ajax({
     url: "http://api.citybik.es/citi-bike-nyc.json",
@@ -82,7 +90,7 @@ var findCitiBike = function() {
   });
 }
 
-
+//Loops through Citibike data, finds closest stations to origin and destination addresses
 var findClosestStation = function(data) {
 
   haversineResult = undefined;
@@ -90,6 +98,7 @@ var findClosestStation = function(data) {
   originStationLng = undefined;
   shortestOriginDistance = 10000;
 
+  //first for loop, finds "origin station"
   for (var i = 0; i < data.length; i++) {
     var citiLat = (data[i]["lat"]).toString()
     formatCitiCoords(citiLat);
@@ -102,9 +111,10 @@ var findClosestStation = function(data) {
         shortestOriginDistance = haversineResult;
         originStationLat = stationLat;
         originStationLng = stationLng;
+        stationData.originName = data[i]["name"]
+        stationData.numBikes = data[i]["bikes"]
       }
     }
-
   }
   console.log("the closest citibike dock to the origin point with available bikes is at coords: " + originStationLat + ", " + originStationLng)
   fullOriginCoords = originStationLat + ", " + originStationLng;
@@ -114,6 +124,7 @@ var findClosestStation = function(data) {
   destinationStationLng = undefined;
   shortestDestinationDistance = 10000;
 
+  //second for loop, finds "destination station"
   for (var i = 0; i < data.length; i++) {
     var citiLat = (data[i]["lat"]).toString()
     formatCitiCoords(citiLat);
@@ -126,15 +137,28 @@ var findClosestStation = function(data) {
         shortestDestinationDistance = haversineResult;
         destinationStationLat = stationLat;
         destinationStationLng = stationLng;
+        stationData.destinationName = data[i]["name"]
+        stationData.numSlots = data[i]["free"]
       }
     }
   }
+  fillHandlebars(stationData)
   console.log("the closest citibike dock to the destination point with available bikes is at coords: " + destinationStationLat + ", " + destinationStationLng)
   fullDestinationCoords = destinationStationLat + ", " + destinationStationLng
 };
 
+//Formats citibike coordinate data so it can be passed into haversine formula
+var formatCitiCoords = function(coord) {
+  if (coord[0] == "-") {
+    var formattedCoord = [coord.slice(0, 3), ".", coord.slice(3)].join('');
+    stationLng = parseFloat(formattedCoord);
+  } else {
+    var formattedCoord = [coord.slice(0, 2), ".", coord.slice(2)].join('');
+    stationLat = parseFloat(formattedCoord);
+  }
+}
 
-//converts lat and lng to radians
+//converts lat and lng to radians for Haversine formula
 toRadians = function(num) {
   return num * Math.PI / 180;
 }
@@ -156,77 +180,99 @@ var haversine = function(lat1, lng1, lat2, lng2) {
   haversineResult = d;
 }
 
-//formats citibike coordinate data so it can be passed into haversine formula
-var formatCitiCoords = function(coord) {
-  if (coord[0] == "-") {
-    var formattedCoord = [coord.slice(0, 3), ".", coord.slice(3)].join('');
-    stationLng = parseFloat(formattedCoord);
-  } else {
-    var formattedCoord = [coord.slice(0, 2), ".", coord.slice(2)].join('');
-    stationLat = parseFloat(formattedCoord);
-  }
-}
-
+//Draws map on page after origin/destination information is filled in and stations have been located.
 function initMap() {
-    var gmaps       = google.maps,
-        map         = new gmaps.Map(document.getElementById('map-canvas'),
-                                  {
-                                    center  : new gmaps.LatLng(40.72, -73.97),
-                                    zoom    : 10
-                                  }
-                                 ),
-        App         = { map               : map,
-                        bounds            : new gmaps.LatLngBounds(),
-                        directionsService : new gmaps.DirectionsService(),
-                        directionsDisplay1: new gmaps.DirectionsRenderer({
-                                              map             : map,
-                                              preserveViewport: true,
-                                              suppressMarkers : true,
-                                              polylineOptions : {strokeColor:'red'},
-                                              panel           : document.getElementById('panel').appendChild(document.createElement('li'))}),
-                        directionsDisplay2: new gmaps.DirectionsRenderer({
-                                              map             : map,
-                                              preserveViewport: true,
-                                              suppressMarkers : true,
-                                              polylineOptions : {strokeColor:'blue'},
-                                              panel           : document.getElementById('panel').appendChild(document.createElement('li'))}),
-                        directionsDisplay3: new gmaps.DirectionsRenderer({
-                                              map             : map,
-                                              preserveViewport: true,
-                                              suppressMarkers : true,
-                                              polylineOptions : {strokeColor:'yellow'},
-                                              panel           : document.getElementById('panel').appendChild(document.createElement('li'))
-                                              })
-
-            },
-         startLeg   = {  origin     :  origin,
-                        destination :  fullOriginCoords,
-                        travelMode  :  gmaps.TravelMode.WALKING},
-         middleLeg  = {  origin     :  fullOriginCoords,
-                        destination :  fullDestinationCoords,
-                        travelMode  :  gmaps.TravelMode.BICYCLING},
-         endLeg     = { origin      :  fullDestinationCoords,
-                        destination :  destination,
-                        travelMode  :  gmaps.TravelMode.WALKING};
-
-      App.directionsService.route(startLeg, function(result, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-          App.directionsDisplay1.setDirections(result);
-          App.map.fitBounds(App.bounds.union(result.routes[0].bounds));
+  var gmaps = google.maps,
+    map = new gmaps.Map(document.getElementById('map-canvas'), {
+      center: new gmaps.LatLng(40.72, -73.97),
+      zoom: 13
+    }),
+    App = {
+      map: map,
+      bounds: new gmaps.LatLngBounds(),
+      directionsService: new gmaps.DirectionsService(),
+      directionsDisplay1: new gmaps.DirectionsRenderer({
+        map: map,
+        preserveViewport: true,
+        suppressMarkers: true,
+        polylineOptions: {
+          strokeColor: 'yellow'
         }
-      });
-
-      App.directionsService.route(middleLeg, function(result, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-          App.directionsDisplay2.setDirections(result);
-          App.map.fitBounds(App.bounds.union(result.routes[0].bounds));
+      }),
+      directionsDisplay2: new gmaps.DirectionsRenderer({
+        map: map,
+        preserveViewport: true,
+        suppressMarkers: true,
+        polylineOptions: {
+          strokeColor: 'blue'
         }
-      });
+      }),
+      directionsDisplay3: new gmaps.DirectionsRenderer({
+        map: map,
+        preserveViewport: true,
+        suppressMarkers: true,
+        polylineOptions: {
+          strokeColor: 'yellow'
+        },
+      })
+    },
+    startLeg = {
+      origin: origin,
+      destination: fullOriginCoords,
+      travelMode: gmaps.TravelMode.WALKING
+    },
+    middleLeg = {
+      origin: fullOriginCoords,
+      destination: fullDestinationCoords,
+      travelMode: gmaps.TravelMode.BICYCLING
+    },
+    endLeg = {
+      origin: fullDestinationCoords,
+      destination: destination,
+      travelMode: gmaps.TravelMode.WALKING
+    };
 
-      App.directionsService.route(endLeg, function(result, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-          App.directionsDisplay3.setDirections(result);
-          App.map.fitBounds(App.bounds.union(result.routes[0].bounds));
-        }
-      });
+  App.directionsService.route(startLeg, function(result, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      App.directionsDisplay1.setDirections(result);
+      document.getElementById("one").innerHTML = "";
+      App.directionsDisplay1.setPanel(document.getElementById("one"));
+      App.map.fitBounds(App.bounds.union(result.routes[0].bounds));
+    }
+  });
+
+  App.directionsService.route(middleLeg, function(result, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      App.directionsDisplay2.setDirections(result);
+      document.getElementById("two").innerHTML = "";
+      App.directionsDisplay2.setPanel(document.getElementById("two"));
+      App.map.fitBounds(App.bounds.union(result.routes[0].bounds));
+    }
+  });
+
+  App.directionsService.route(endLeg, function(result, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      App.directionsDisplay3.setDirections(result);
+      document.getElementById("three").innerHTML = "";
+      App.directionsDisplay3.setPanel(document.getElementById("three"));
+      App.map.fitBounds(App.bounds.union(result.routes[0].bounds));
+    }
+  });
 }
+
+//Fills handlebars template
+var fillHandlebars = function(object) {
+  handleBarsContainer.innerHTML = "";
+  var templateSource = document.getElementById('handlebarsTemplate').innerHTML;
+  var template = Handlebars.compile(templateSource);
+  var container = document.getElementById('handlebars-container');
+  var computedHtml = template(object);
+  var filledTemplate = document.createElement("span");
+  filledTemplate.innerHTML = computedHtml;
+  handleBarsContainer.appendChild(filledTemplate);
+}
+
+//Initializes map after all ajax requests are done.
+$(document).ajaxStop(function() {
+  initMap();
+})
